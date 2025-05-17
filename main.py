effe8b5afccf6e043af9b2fb7038588cb9ebc615
Ashley Panelo <118011180+Ashl3y05@users.noreply.github.com>
2025-05-18 04:25:19 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#Variables
+name = input("What is your name?") #Equal sign is an assignment operator to assign a value to a variable
+print(name) #printing the value of the variable "name"
+
+name = "I'll change your name to Donut" #changes the last value of the variable "name"
+print(name)
+
+#make variable names meaningful and descriptive as much as possible
+#use underscores,uppercase, or numbers to separate words in a variable name
+#DON'T use space when naming variable
+#DON'T use reserved words as variable names
+#DON'T use special characters in variable names
+#DON'T use numbers as the first character in a variable name
+#ALWAYS define variable names before using them
